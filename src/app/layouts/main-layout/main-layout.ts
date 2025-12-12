1ececbe6cf7769cc8d70cb421e3891769e918f8f
Fonzy57 <traben@hotmail.fr>
2025-12-12 12:04:12 +0100

    Add standalone to layouts components

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 
 @Component({
+  standalone: true,
   selector: "app-main-layout",
   imports: [RouterOutlet],
   templateUrl: "./main-layout.html",
