add75417fe9ed6d75c6feb3c42cd9f8dc1f122a1
Fonzy57 <traben@hotmail.fr>
2025-12-12 11:39:16 +0100

    Add dashboard layout component

---
+++ src/app/layouts/dashboard-layout/dashboard-layout.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+  selector: "app-dashboard-layout",
+  imports: [RouterOutlet],
+  templateUrl: "./dashboard-layout.html",
+})
+export class DashboardLayout {}
