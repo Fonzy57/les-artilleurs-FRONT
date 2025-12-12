f438b40818f677c6a8a009838bab6ad579385562
Fonzy57 <traben@hotmail.fr>
2025-12-12 11:18:22 +0100

    Add button component to home page for testing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
+import { ButtonComponent } from './shared/ui/button/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
   standalone: true,
 })
 export class App {
   protected readonly title = signal('les-artilleurs-FRONT');
+
+  test() {
+    console.log('Je clique sur le bouton');
+  }
 }
