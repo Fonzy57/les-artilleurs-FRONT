cbc0bc49474ddfe2e274f4a7caab35722284b2a6
Fonzy57 <traben@hotmail.fr>
2025-12-14 15:29:08 +0100

    Add a config file for footer documents

---
+++ src/app/core/config/documents.ts
@@ -0,0 +1,12 @@
+type doumentsUtil = {
+  name: string;
+  label: string;
+  url: string;
+};
+
+export const documentsUtil: doumentsUtil[] = [
+  { name: "licence", label: "Dossiers de Licence", url: "#" },
+  { name: "informations", label: "Fiche de renseignement", url: "#" },
+  { name: "legal-notice", label: "Mentions légales", url: "#" },
+  { name: "privacy-policy", label: "Politique de confidentialité", url: "#" },
+];
