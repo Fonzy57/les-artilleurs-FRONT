bf86f22d7e9a1aa19164228436c8f8aed49337b8
Fonzy57 <traben@hotmail.fr>
2025-12-14 17:06:01 +0100

    Add category section to home page

--- src/app/pages/website/home/home.ts
+++ src/app/pages/website/home/home.ts
@@ -3,11 +3,12 @@ import { Component } from "@angular/core";
 
 // SECTIONS
 import { Hero } from "@layouts/main-layout/ui/sections/hero/hero";
+import { Category } from "@layouts/main-layout/ui/sections/category/category";
 
 @Component({
   standalone: true,
   selector: "app-home",
-  imports: [Hero],
+  imports: [Hero, Category],
   templateUrl: "./home.html",
 })
 export class Home {}
