289398dbcbd0f52029fe9a6dec41e7a019aa7bf1
Fonzy57 <traben@hotmail.fr>
2025-12-14 17:14:40 +0100

    Add registration section component to website home page

--- src/app/pages/website/home/home.ts
+++ src/app/pages/website/home/home.ts
@@ -4,11 +4,12 @@ import { Component } from "@angular/core";
 // SECTIONS
 import { Hero } from "@layouts/main-layout/ui/sections/hero/hero";
 import { Category } from "@layouts/main-layout/ui/sections/category/category";
+import { Registration } from "@layouts/main-layout/ui/sections/registration/registration";
 
 @Component({
   standalone: true,
   selector: "app-home",
-  imports: [Hero, Category],
+  imports: [Hero, Category, Registration],
   templateUrl: "./home.html",
 })
 export class Home {}
