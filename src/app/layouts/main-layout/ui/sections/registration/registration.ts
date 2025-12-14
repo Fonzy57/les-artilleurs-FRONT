d35cb38c1c24e7fb070170edb7d5d938c2c782ca
Fonzy57 <traben@hotmail.fr>
2025-12-14 17:14:14 +0100

    Add rehistration section component with content, no style

---
+++ src/app/layouts/main-layout/ui/sections/registration/registration.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { ButtonComponent } from "@shared/ui/button/button";
+
+@Component({
+  selector: "app-website-registration",
+  imports: [ButtonComponent],
+  templateUrl: "./registration.html",
+  styleUrl: "./registration.css",
+})
+export class Registration {}
