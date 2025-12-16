6f32760e768ab22c22739e57681cb608bf986c07
Fonzy57 <traben@hotmail.fr>
2025-12-16 11:18:47 +0100

    Add history section component to homepage

--- src/app/pages/website/home/home.ts
+++ src/app/pages/website/home/home.ts
@@ -5,11 +5,12 @@ import { Component } from "@angular/core";
 import { Hero } from "@layouts/main-layout/ui/sections/hero/hero";
 import { Category } from "@layouts/main-layout/ui/sections/category/category";
 import { Registration } from "@layouts/main-layout/ui/sections/registration/registration";
+import { History } from "@layouts/main-layout/ui/sections/history/history";
 
 @Component({
   standalone: true,
   selector: "app-home",
-  imports: [Hero, Category, Registration],
+  imports: [Hero, Category, Registration, History],
   templateUrl: "./home.html",
 })
 export class Home {}
