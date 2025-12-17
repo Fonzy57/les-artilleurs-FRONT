78f7e0a7413b7022f19daa4ba8e04ce9f436f23e
Fonzy57 <traben@hotmail.fr>
2025-12-17 17:13:09 +0100

    Add toast component to app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
+// ANGULAR
 import { Component, signal } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 
+// COMPONENTS
+import { AppToastComponent } from "@shared/ui/toast/toast";
+
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppToastComponent],
   templateUrl: "./app.html",
   styleUrl: "./app.css",
   standalone: true,
