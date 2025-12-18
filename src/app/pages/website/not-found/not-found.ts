a01b4427c0ab5bd514c408bdc348ec5468c253a9
Fonzy57 <traben@hotmail.fr>
2025-12-18 12:40:17 +0100

    Add not found page

---
+++ src/app/pages/website/not-found/not-found.ts
@@ -0,0 +1,12 @@
+// ANGULAR
+import { Component } from "@angular/core";
+
+// COMPONENTS
+import { ButtonComponent } from "@shared/ui/button/button";
+
+@Component({
+  selector: "app-not-found",
+  imports: [ButtonComponent],
+  templateUrl: "./not-found.html",
+})
+export class NotFound {}
