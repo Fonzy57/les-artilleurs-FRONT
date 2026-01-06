a539b0c2b0df0fdff099582b48790a0c116d22c2
Fonzy57 <traben@hotmail.fr>
2026-01-06 12:06:50 +0100

    Add api url in global config

--- src/app/core/config/app.config.ts
+++ src/app/core/config/app.config.ts
@@ -1,5 +1,6 @@
 export const artilleursConfig = {
   version: "0.0.1",
+  apiUrl: "http://localhost:8080/api", // TODO CHANGER QUAND J'AURAIS LE NOM DE DOMAINE
   baseUrl: "https://les-artilleurs.fr", // TODO CHANGER QUAND J'AURAIS LE NOM DE DOMAINE
   mail: "contact@les-artilleurs.fr", // TODO CHANGER QUAND J'AURAIS LE NOM DE DOMAINE
 };
