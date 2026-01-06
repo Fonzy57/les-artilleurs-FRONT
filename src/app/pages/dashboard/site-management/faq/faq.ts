36faafec644b5512653db3e51721aaa4106afc77
Fonzy57 <traben@hotmail.fr>
2026-01-06 15:46:08 +0100

    Add faq management component

---
+++ src/app/pages/dashboard/site-management/faq/faq.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  standalone: true,
+  selector: "app-management-faq",
+  imports: [],
+  templateUrl: "./faq.html",
+})
+export class FaqManagement {}
