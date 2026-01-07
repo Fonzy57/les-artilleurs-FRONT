806baf24193f94f8c47f9284db74edcfd2263dad
Fonzy57 <traben@hotmail.fr>
2026-01-07 12:29:27 +0100

    Update faq section for admin dashboard with primeng table

--- src/app/pages/dashboard/site-management/faq/faq.ts
+++ src/app/pages/dashboard/site-management/faq/faq.ts
@@ -1,9 +1,41 @@
-import { Component } from "@angular/core";
+// ANGULAR
+import { CommonModule } from "@angular/common";
+import { Component, inject, OnInit } from "@angular/core";
+
+// PRIME NG
+import { TableModule } from "primeng/table";
+
+// SERVICE
+import { FaqAdminService } from "app/data-access/admin/faq/faq-admin.service";
+
+// COMPONENTS
+import {
+  ActionIconButton,
+  ActionIconButtonType,
+} from "@layouts/dashboard-layout/ui/action-icon-button/action-icon-button";
 
 @Component({
   standalone: true,
   selector: "app-management-faq",
-  imports: [],
+  imports: [TableModule, CommonModule, ActionIconButton],
   templateUrl: "./faq.html",
 })
-export class FaqManagement {}
+export class FaqManagement implements OnInit {
+  readonly faqService = inject(FaqAdminService);
+
+  ngOnInit(): void {
+    this.faqService.loadFaqItems();
+  }
+
+  onEdit(id: number) {
+    console.log("je clique sur MODIFIER un élément avec l'id : " + id);
+  }
+
+  onDelete(id: number) {
+    console.log("je clique sur SUPPRIMER un élément avec l'id : " + id);
+  }
+
+  onView(id: number) {
+    console.log("je clique sur VOIR un élément avec l'id : " + id);
+  }
+}
