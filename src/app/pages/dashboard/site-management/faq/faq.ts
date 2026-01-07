d51aef6a477c402e478cdd2bb5911cdb16028023
Fonzy57 <traben@hotmail.fr>
2026-01-07 14:29:14 +0100

    Add faq table skeleton and error display

--- src/app/pages/dashboard/site-management/faq/faq.ts
+++ src/app/pages/dashboard/site-management/faq/faq.ts
@@ -4,20 +4,29 @@ import { Component, inject, OnInit } from "@angular/core";
 
 // PRIME NG
 import { TableModule } from "primeng/table";
+import { ConfirmDialog } from "primeng/confirmdialog";
 
 // SERVICE
 import { FaqAdminService } from "app/data-access/admin/faq/faq-admin.service";
 
 // COMPONENTS
-import {
-  ActionIconButton,
-  ActionIconButtonType,
-} from "@layouts/dashboard-layout/ui/action-icon-button/action-icon-button";
+import { ActionIconButton } from "@layouts/dashboard-layout/ui/action-icon-button/action-icon-button";
+import { TableSkeleton } from "@shared/ui/skeleton/table-skeleton/table-skeleton";
+import { AlertCard } from "@shared/ui/alert-card/alert-card";
+import { ButtonComponent } from "@shared/ui/button/button";
 
 @Component({
   standalone: true,
   selector: "app-management-faq",
-  imports: [TableModule, CommonModule, ActionIconButton],
+  imports: [
+    TableModule,
+    CommonModule,
+    ActionIconButton,
+    ConfirmDialog,
+    TableSkeleton,
+    AlertCard,
+    ButtonComponent,
+  ],
   templateUrl: "./faq.html",
 })
 export class FaqManagement implements OnInit {
@@ -28,14 +37,12 @@ export class FaqManagement implements OnInit {
   }
 
   onEdit(id: number) {
+    /* TODO OUVRIR UNE MODAL POUR FAIRE LA MODIFICATION */
     console.log("je clique sur MODIFIER un élément avec l'id : " + id);
   }
 
   onDelete(id: number) {
+    /* TODO MODAL DE VALIDATION POUR LA SUPPRESION AVEC MESSAGE CLAIR */
     console.log("je clique sur SUPPRIMER un élément avec l'id : " + id);
-  }
-
-  onView(id: number) {
-    console.log("je clique sur VOIR un élément avec l'id : " + id);
   }
 }
