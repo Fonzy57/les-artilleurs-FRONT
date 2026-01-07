ed980e0f1461db79d3f4038975b62bce321571c0
Fonzy57 <traben@hotmail.fr>
2026-01-07 10:22:31 +0100

    Add alert card component to site management page and use lazy loading for tab content with ng-template

--- src/app/pages/dashboard/site-management/site-management.ts
+++ src/app/pages/dashboard/site-management/site-management.ts
@@ -9,9 +9,18 @@ import { ClubInfosManagement } from "./club-infos/club-infos";
 import { FaqManagement } from "./faq/faq";
 import { InfosManagement } from "./infos/infos";
 
+// COMPONENTS
+import { AlertCard } from "@shared/ui/alert-card/alert-card";
+
 @Component({
   selector: "app-dashboard-site-management",
-  imports: [TabsModule, ClubInfosManagement, FaqManagement, InfosManagement],
+  imports: [
+    TabsModule,
+    ClubInfosManagement,
+    FaqManagement,
+    InfosManagement,
+    AlertCard,
+  ],
   templateUrl: "./site-management.html",
 })
 export class SiteManagement {}
