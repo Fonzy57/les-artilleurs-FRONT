944cb1d47d10a8160948b2feeb48c9d092021f02
Fonzy57 <traben@hotmail.fr>
2026-01-08 12:38:42 +0100

    Add confirm dialog for deleting an faq item

--- src/app/pages/dashboard/site-management/faq/faq.ts
+++ src/app/pages/dashboard/site-management/faq/faq.ts
@@ -5,15 +5,21 @@ import { Component, inject, OnInit } from "@angular/core";
 // PRIME NG
 import { TableModule } from "primeng/table";
 import { ConfirmDialog } from "primeng/confirmdialog";
+import { ConfirmationService } from "primeng/api";
 
 // SERVICE
 import { FaqAdminService } from "app/data-access/admin/faq/faq-admin.service";
+import { ToastService } from "@shared/ui/toast/toast.service";
 
 // COMPONENTS
 import { ActionIconButton } from "@layouts/dashboard-layout/ui/action-icon-button/action-icon-button";
 import { TableSkeleton } from "@shared/ui/skeleton/table-skeleton/table-skeleton";
 import { AlertCard } from "@shared/ui/alert-card/alert-card";
 import { ButtonComponent } from "@shared/ui/button/button";
+import { DashButton } from "@layouts/dashboard-layout/ui/dash-button/dash-button";
+
+// MODELS
+import { FaqAdmin } from "@shared/models/faq.model";
 
 @Component({
   standalone: true,
@@ -26,23 +32,45 @@ import { ButtonComponent } from "@shared/ui/button/button";
     TableSkeleton,
     AlertCard,
     ButtonComponent,
+    DashButton,
   ],
   templateUrl: "./faq.html",
 })
 export class FaqManagement implements OnInit {
   readonly faqService = inject(FaqAdminService);
+  private toast = inject(ToastService);
+  confirmationService = inject(ConfirmationService);
 
   ngOnInit(): void {
     this.faqService.loadFaqItems();
   }
 
-  onEdit(id: number) {
-    /* TODO OUVRIR UNE MODAL POUR FAIRE LA MODIFICATION */
-    console.log("je clique sur MODIFIER un élément avec l'id : " + id);
+  /* --- TABLE ACTIONS --- */
+  onEdit(faqItem: FaqAdmin): void {
+    console.log("Je clique sur éditer");
+    /* TODO AUTRE MODAL */
   }
 
-  onDelete(id: number) {
-    /* TODO MODAL DE VALIDATION POUR LA SUPPRESION AVEC MESSAGE CLAIR */
-    console.log("je clique sur SUPPRIMER un élément avec l'id : " + id);
+  onDelete(faqItem: FaqAdmin): void {
+    this.confirmationService.confirm({
+      key: "faq-delete",
+      header: "Supprimer l’élément de la FAQ",
+      message: `${faqItem.question}`,
+      accept: () => {
+        this.onDeleteFaqItem(faqItem);
+      },
+      reject: () => {
+        this.toast.info(
+          "Suppression annulée",
+          `L'élément de la FAQ "${faqItem.question}" n'a pas été supprimé.`,
+        );
+      },
+    });
+  }
+
+  /* --- DIALOG ACTIONS --- */
+  onDeleteFaqItem(faqItem: FaqAdmin): void {
+    console.log("Je supprime l'élément avec l'id : " + faqItem.id);
+    /* TODO ICI LE SERVICE POUR FAIRE LA SUPPRESSION */
   }
 }
