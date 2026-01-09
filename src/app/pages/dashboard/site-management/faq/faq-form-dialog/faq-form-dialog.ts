4b99ad77c5370a03eab75bf8ea4990571ef1cdca
Fonzy57 <traben@hotmail.fr>
2026-01-09 12:56:14 +0100

    Rename faqEditDialog component to faqFormDialog because use for adding and editing a faq item

--- src/app/pages/dashboard/site-management/faq/faq-form-dialog/faq-form-dialog.ts
+++ src/app/pages/dashboard/site-management/faq/faq-form-dialog/faq-form-dialog.ts
@@ -1,5 +1,12 @@
 // ANGULAR
-import { Component, effect, inject, input, output } from "@angular/core";
+import {
+  Component,
+  computed,
+  effect,
+  inject,
+  input,
+  output,
+} from "@angular/core";
 import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
 import { NgClass } from "@angular/common";
 
@@ -13,14 +20,16 @@ import { DashButton } from "@layouts/dashboard-layout/ui/dash-button/dash-button
 import { FaqAdmin } from "@shared/models/faq.model";
 
 // TYPES
-export type FaqEditPayload = {
+export type FaqFormMode = "create" | "edit";
+
+export type FaqFormPayload = {
   question: string;
   answer: string;
 };
 
 @Component({
   standalone: true,
-  selector: "app-faq-edit-dialog",
+  selector: "app-faq-form-dialog",
   imports: [
     Dialog,
     ReactiveFormsModule,
@@ -29,22 +38,33 @@ export type FaqEditPayload = {
     DashButton,
     NgClass,
   ],
-  templateUrl: "./faq-edit-dialog.html",
+  templateUrl: "./faq-form-dialog.html",
 })
-export class FaqEditDialog {
+export class FaqFormDialog {
   private readonly formBuilder = inject(FormBuilder);
   readonly QUESTION_MAX = 255;
   readonly RESPONSE_MAX = 1500;
 
   // inputs
   visible = input<boolean>(false);
+  mode = input<FaqFormMode>("edit");
   item = input<FaqAdmin | null>(null);
   loading = input<boolean>(false);
 
   // outputs
   visibleChange = output<boolean>();
   cancel = output<void>();
-  save = output<FaqEditPayload>();
+  save = output<FaqFormPayload>();
+
+  readonly textHeader = computed(() => {
+    return this.mode() === "create"
+      ? "Ajouter un élément à la FAQ"
+      : "Modifier l'élément de la FAQ";
+  });
+
+  readonly submitLabel = computed(() => {
+    return this.mode() === "create" ? "Ajouter" : "Modifier";
+  });
 
   readonly form = this.formBuilder.nonNullable.group({
     question: [
@@ -59,6 +79,17 @@ export class FaqEditDialog {
 
   constructor() {
     effect(() => {
+      const mode = this.mode();
+
+      // Create mode
+      if (mode === "create") {
+        this.form.reset();
+        this.form.markAsPristine();
+        this.form.markAsUntouched();
+        return;
+      }
+
+      // Edit mode
       const item = this.item();
       if (!item) return;
 
@@ -71,6 +102,7 @@ export class FaqEditDialog {
       this.form.markAsUntouched();
     });
 
+    // Disable form if loading
     effect(() => {
       if (this.loading()) {
         this.form.disable({ emitEvent: false });
