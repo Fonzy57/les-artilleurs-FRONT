38e489d576677cb513b00ee43eff7d2165769330
Fonzy57 <traben@hotmail.fr>
2026-01-09 11:11:44 +0100

    Add faq edit dialog component in faq management

--- src/app/pages/dashboard/site-management/faq/faq.ts
+++ src/app/pages/dashboard/site-management/faq/faq.ts
@@ -1,21 +1,11 @@
 // ANGULAR
 import { CommonModule } from "@angular/common";
-import { Component, effect, inject, OnInit, signal } from "@angular/core";
-import {
-  FormBuilder,
-  FormsModule,
-  ReactiveFormsModule,
-  Validators,
-} from "@angular/forms";
+import { Component, inject, OnInit } from "@angular/core";
 
 // PRIME NG
 import { TableModule } from "primeng/table";
 import { ConfirmDialog } from "primeng/confirmdialog";
 import { ConfirmationService } from "primeng/api";
-import { Dialog } from "primeng/dialog";
-import { ButtonModule } from "primeng/button";
-import { InputTextModule } from "primeng/inputtext";
-import { TextareaModule } from "primeng/textarea";
 
 // SERVICE
 import { FaqAdminService } from "app/data-access/admin/faq/faq-admin.service";
@@ -27,6 +17,10 @@ import { TableSkeleton } from "@shared/ui/skeleton/table-skeleton/table-skeleton
 import { AlertCard } from "@shared/ui/alert-card/alert-card";
 import { ButtonComponent } from "@shared/ui/button/button";
 import { DashButton } from "@layouts/dashboard-layout/ui/dash-button/dash-button";
+import {
+  FaqEditPayload,
+  FaqEditDialog,
+} from "./faq-edit-dialog/faq-edit-dialog";
 
 // MODELS
 import { FaqAdmin } from "@shared/models/faq.model";
@@ -43,29 +37,11 @@ import { FaqAdmin } from "@shared/models/faq.model";
     AlertCard,
     ButtonComponent,
     DashButton,
-    Dialog,
-    ButtonModule,
-    InputTextModule,
-    FormsModule,
-    ReactiveFormsModule,
-    TextareaModule,
+    FaqEditDialog,
   ],
   templateUrl: "./faq.html",
 })
 export class FaqManagement implements OnInit {
-  constructor() {
-    effect(() => {
-      const item = this.faqService.selectedFaqItem();
-
-      if (!item) return;
-
-      this.editFaqItemForm.patchValue({
-        question: item.question,
-        response: item.answer,
-      });
-    });
-  }
-
   readonly faqService = inject(FaqAdminService);
   private toast = inject(ToastService);
   confirmationService = inject(ConfirmationService);
@@ -75,24 +51,27 @@ export class FaqManagement implements OnInit {
     this.faqService.loadFaqItems();
   }
 
-  /* --- FORM --- */
-  formBuilder: FormBuilder = inject(FormBuilder);
-
-  editFaqItemForm = this.formBuilder.group({
-    question: ["", Validators.required],
-    response: ["", Validators.required],
-  });
-
-  isInvalid(controlName: string) {
-    const control = this.editFaqItemForm.get(controlName);
-    return !!(control && control.invalid);
-  }
-  /* --- END FORM --- */
-
   /* --- TABLE ACTIONS --- */
   onShowEditDialog(faqItem: FaqAdmin): void {
     this.faqService.getOneFaqItem(faqItem.id);
     this.visible = true;
+  }
+
+  onEditCancel(): void {
+    this.visible = false;
+    this.faqService.clearSelected();
+  }
+
+  onEditSave(editFormData: FaqEditPayload): void {
+    const current = this.faqService.selectedFaqItem();
+    if (!current) return;
+
+    this.faqService.editFaqItem(current.id, editFormData).subscribe({
+      next: () => {
+        this.onEditCancel();
+      },
+      error: () => {},
+    });
   }
 
   onDelete(faqItem: FaqAdmin): void {
@@ -115,28 +94,5 @@ export class FaqManagement implements OnInit {
   /* --- DIALOG ACTIONS --- */
   onDeleteFaqItem(faqItem: FaqAdmin): void {
     this.faqService.deleteFaqItem(faqItem);
-  }
-
-  onCancel() {
-    this.visible = false;
-    this.editFaqItemForm.reset();
-    this.faqService.selectedFaqItem.set(null);
-  }
-
-  onEditItem() {
-    if (this.editFaqItemForm.invalid) {
-      this.editFaqItemForm.markAllAsTouched();
-      return;
-    }
-
-    const item = this.faqService.selectedFaqItem();
-    if (!item) return;
-
-    const { question, response } = this.editFaqItemForm.getRawValue();
-
-    // ici tu appelleras ton service PUT
-    // this.faqService.editFaqItem({ ...item, question, answer: response });
-
-    console.log("submit", item.id, question, response);
   }
 }
