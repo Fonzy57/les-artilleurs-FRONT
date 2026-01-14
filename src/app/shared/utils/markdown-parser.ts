bfa7927845dd338fabd3e330f0b304b07b482ac1
Fonzy57 <traben@hotmail.fr>
2026-01-14 13:01:49 +0100

    Add a markdown parser in utils folder

---
+++ src/app/shared/utils/markdown-parser.ts
@@ -0,0 +1,46 @@
+export type InfoBlockChunk = {
+  text: string;
+  isBold: boolean;
+  isItalic: boolean;
+};
+
+// ONLY for bold '**' and italic '_'
+export function markdownParser(content: string): InfoBlockChunk[] {
+  const arrayOfChunks: InfoBlockChunk[] = [];
+  let isBold = false;
+  let isItalic = false;
+  let buffer = "";
+
+  for (let i = 0; i < content.length; i++) {
+    const char = content[i];
+
+    // Bold text
+    if (content.startsWith("**", i)) {
+      if (buffer.trim().length > 0) {
+        arrayOfChunks.push({ text: buffer.trimEnd(), isBold, isItalic });
+      }
+      buffer = "";
+      isBold = !isBold;
+      i++;
+      continue;
+    }
+
+    // Italic text
+    if (content.startsWith("_", i)) {
+      if (buffer.trim().length > 0) {
+        arrayOfChunks.push({ text: buffer.trimEnd(), isBold, isItalic });
+      }
+      buffer = "";
+      isItalic = !isItalic;
+      continue;
+    }
+    // Normal text
+    buffer += char;
+  }
+
+  if (buffer.trim().length > 0) {
+    arrayOfChunks.push({ text: buffer.trimStart(), isBold, isItalic });
+  }
+
+  return arrayOfChunks;
+}
