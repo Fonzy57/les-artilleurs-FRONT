91a346c6f6d42f730bd3433b6cfab9bc82022fda
Fonzy57 <traben@hotmail.fr>
2026-01-15 13:35:10 +0100

    Change info preview with same API call

--- src/app/pages/dashboard/site-management/infos/info-preview/info-preview.ts
+++ src/app/pages/dashboard/site-management/infos/info-preview/info-preview.ts
@@ -1,15 +1,16 @@
 // ANGULAR
 import { NgClass } from "@angular/common";
-import { Component, computed, inject, OnInit } from "@angular/core";
+import { Component, computed, input } from "@angular/core";
 
-// SERVICE
-import { InfoBlockService } from "app/data-access/public/info-block/info-block.service";
-
-// TYPES
-import { InfoBlockPublicParsed } from "@layouts/main-layout/ui/sections/info-block/info-block";
+// MODELS
+import { InfoBlockAdmin } from "@shared/models/info-block.model";
 
 // UTILS
-import { markdownParser } from "@shared/utils/markdown-parser";
+import { InfoBlockChunk, markdownParser } from "@shared/utils/markdown-parser";
+
+type InfoBlockAdminPreviewParsed = InfoBlockAdmin & {
+  chunks: InfoBlockChunk[];
+};
 
 @Component({
   standalone: true,
@@ -17,19 +18,21 @@ import { markdownParser } from "@shared/utils/markdown-parser";
   imports: [NgClass],
   templateUrl: "./info-preview.html",
 })
-export class InfoPreview implements OnInit {
-  readonly infoBlockPublicService = inject(InfoBlockService);
+export class InfoPreview {
+  infoBlocks = input<InfoBlockAdmin[]>([]);
+  loading = input<boolean>(false);
+  error = input<boolean>(false);
 
-  parsedInfoBlocks = computed<InfoBlockPublicParsed[]>(() => {
-    const blocks = this.infoBlockPublicService.infoBlocks();
-
-    return blocks.map((block) => ({
-      ...block,
-      chunks: markdownParser(block.content),
-    }));
+  parsedInfoBlocks = computed<InfoBlockAdminPreviewParsed[]>(() => {
+    const blocks = this.infoBlocks();
+    return blocks
+      .filter((block) => block.slot !== null)
+      .sort((a, b) => a.slot! - b.slot!)
+      .map((block) => {
+        return {
+          ...block,
+          chunks: markdownParser(block.content),
+        };
+      });
   });
-
-  ngOnInit(): void {
-    this.infoBlockPublicService.loadInfoBlocks();
-  }
 }
