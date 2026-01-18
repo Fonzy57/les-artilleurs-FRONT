b4f8fdae16cf0c0f926bef180100346c228c6043
Fonzy57 <traben@hotmail.fr>
2026-01-18 15:51:26 +0100

    Add club info page structure and style

--- src/app/pages/dashboard/site-management/club-infos/club-infos.ts
+++ src/app/pages/dashboard/site-management/club-infos/club-infos.ts
@@ -1,9 +1,41 @@
-import { Component } from "@angular/core";
+// ANGULAR
+import { Component, inject, OnInit } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+
+// PRIME NG
+import { InputTextModule } from "primeng/inputtext";
+import { DividerModule } from "primeng/divider";
+
+// COMPONENTS
+import { DashButton } from "@layouts/dashboard-layout/ui/dash-button/dash-button";
+import { ActionIconButton } from "@layouts/dashboard-layout/ui/action-icon-button/action-icon-button";
+import { InputSkeleton } from "@shared/ui/skeleton/form/input-skeleton/input-skeleton";
+
+// SERVICE
+import { ClubInfoAdminService } from "app/data-access/admin/club/club-info-admin.service";
 
 @Component({
   standalone: true,
   selector: "app-management-club-infos",
-  imports: [],
+  imports: [
+    InputTextModule,
+    InputSkeleton,
+    DashButton,
+    ActionIconButton,
+    DividerModule,
+  ],
   templateUrl: "./club-infos.html",
 })
-export class ClubInfosManagement {}
+export class ClubInfosManagement implements OnInit {
+  readonly clubInfoAdminService = inject(ClubInfoAdminService);
+
+  readonly infos = this.clubInfoAdminService.clubInfos;
+
+  ngOnInit(): void {
+    this.clubInfoAdminService.loadingClubInfos();
+  }
+
+  clickEdit(): void {
+    console.log("Je clique sur modifier - CLUB INFOS");
+  }
+}
