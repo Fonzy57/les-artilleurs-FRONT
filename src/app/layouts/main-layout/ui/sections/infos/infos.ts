8a4eac286b157200882553dd3d8b15f0d919236a
Fonzy57 <traben@hotmail.fr>
2026-01-18 14:55:32 +0100

    Add club info from API in Info section on home page

--- src/app/layouts/main-layout/ui/sections/infos/infos.ts
+++ src/app/layouts/main-layout/ui/sections/infos/infos.ts
@@ -1,4 +1,8 @@
-import { Component } from "@angular/core";
+// ANGULAR
+import { Component, computed, inject, OnInit } from "@angular/core";
+
+// SERVICE
+import { ClubInfoService } from "app/data-access/public/club/club-info.service";
 
 @Component({
   standalone: true,
@@ -6,4 +10,22 @@ import { Component } from "@angular/core";
   imports: [],
   templateUrl: "./infos.html",
 })
-export class Infos {}
+export class Infos implements OnInit {
+  readonly clubInfoService = inject(ClubInfoService);
+
+  readonly clubInfo = this.clubInfoService.clubInfos;
+
+  readonly address = computed(() => {
+    const info = this.clubInfo();
+    return {
+      stadiumName: info?.stadiumName ?? "Yacine CHERRADI",
+      street: info?.street ?? "Rue de la Grange-Aux-Bois",
+      city: info?.city ?? "Metz",
+      postalCode: info?.postalCode ?? "57070",
+    };
+  });
+
+  ngOnInit(): void {
+    this.clubInfoService.loadingClubInfo();
+  }
+}
