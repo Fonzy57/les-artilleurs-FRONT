dda2cbdcf5176063c08de54dbfed1f7da4fd4aea
Fonzy57 <traben@hotmail.fr>
2026-01-19 13:52:12 +0100

    Add functional reactive form

--- src/app/pages/dashboard/site-management/club-infos/club-infos.ts
+++ src/app/pages/dashboard/site-management/club-infos/club-infos.ts
@@ -1,6 +1,6 @@
 // ANGULAR
-import { Component, inject, OnInit } from "@angular/core";
-import { ReactiveFormsModule } from "@angular/forms";
+import { Component, effect, inject, OnInit, signal } from "@angular/core";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
 
 // PRIME NG
 import { InputTextModule } from "primeng/inputtext";
@@ -9,33 +9,162 @@ import { DividerModule } from "primeng/divider";
 // COMPONENTS
 import { DashButton } from "@layouts/dashboard-layout/ui/dash-button/dash-button";
 import { ActionIconButton } from "@layouts/dashboard-layout/ui/action-icon-button/action-icon-button";
-import { InputSkeleton } from "@shared/ui/skeleton/form/input-skeleton/input-skeleton";
+import { ClubFormSkeleton } from "./club-form-skeleton/club-form-skeleton";
 
 // SERVICE
 import { ClubInfoAdminService } from "app/data-access/admin/club/club-info-admin.service";
+
+// UTILS
+import { requiredAndTrim } from "@shared/validators/trim-required.validator";
 
 @Component({
   standalone: true,
   selector: "app-management-club-infos",
   imports: [
     InputTextModule,
-    InputSkeleton,
+    DividerModule,
     DashButton,
     ActionIconButton,
-    DividerModule,
+    ReactiveFormsModule,
+    ClubFormSkeleton,
   ],
   templateUrl: "./club-infos.html",
 })
 export class ClubInfosManagement implements OnInit {
+  private readonly formBuilder = inject(FormBuilder);
   readonly clubInfoAdminService = inject(ClubInfoAdminService);
-
   readonly infos = this.clubInfoAdminService.clubInfos;
+
+  readonly STADIUM_NAME_LENGTH = 100;
+  readonly STREET_LENGTH = 150;
+  readonly CITY_LENGTH = 80;
+  readonly POSTAL_CODE_LENGTH = 5;
+  readonly CLUB_NAME_LENGTH = 100;
+  readonly CONTACT_EMAIL_LENGTH = 150;
+
+  editIsClicked = signal<boolean>(false);
 
   ngOnInit(): void {
     this.clubInfoAdminService.loadingClubInfos();
   }
 
+  readonly clubForm = this.formBuilder.nonNullable.group({
+    stadiumName: [
+      "",
+      [
+        requiredAndTrim,
+        Validators.maxLength(this.STADIUM_NAME_LENGTH),
+        Validators.required,
+      ],
+    ],
+    street: [
+      "",
+      [
+        requiredAndTrim,
+        Validators.maxLength(this.STREET_LENGTH),
+        Validators.required,
+      ],
+    ],
+    city: [
+      "",
+      [
+        requiredAndTrim,
+        Validators.maxLength(this.CITY_LENGTH),
+        Validators.required,
+      ],
+    ],
+    postalCode: [
+      "",
+      [
+        requiredAndTrim,
+        Validators.maxLength(this.POSTAL_CODE_LENGTH),
+        Validators.required,
+      ],
+    ],
+    clubName: [
+      "",
+      [
+        requiredAndTrim,
+        Validators.maxLength(this.CLUB_NAME_LENGTH),
+        Validators.required,
+      ],
+    ],
+    contactEmail: [
+      "",
+      [
+        requiredAndTrim,
+        Validators.email,
+        Validators.maxLength(this.CONTACT_EMAIL_LENGTH),
+        Validators.required,
+      ],
+    ],
+  });
+
+  constructor() {
+    effect(() => {
+      const data = this.infos();
+      if (!data) return;
+
+      this.clubForm.patchValue(
+        {
+          stadiumName: data.stadiumName,
+          street: data.street,
+          city: data.city,
+          postalCode: data.postalCode,
+          clubName: data.clubName,
+          contactEmail: data.contactEmail,
+        },
+        { emitEvent: false },
+      );
+
+      this.clubForm.markAsPristine();
+      this.clubForm.markAsUntouched();
+      this.clubForm.disable({ emitEvent: false });
+    });
+  }
+
+  /* 
+    TODO
+    - BIEN FAIRE LA VERIFICATION DE L'EMAIL COMME INPUT
+  */
   clickEdit(): void {
-    console.log("Je clique sur modifier - CLUB INFOS");
+    this.editIsClicked.set(true);
+    this.clubForm.enable();
+  }
+
+  handleSubmit(): void {
+    /* TODO VOIR QUAND JE RENDS LE FORMULAIRE DISABLE, A MON AVIS QUE S'IL N'Y A PAS D'ERREURS */
+    this.editIsClicked.set(false);
+    console.log(
+      "J'envoie le formulaire avec comme données : ",
+      this.clubForm.value,
+    );
+  }
+
+  handleCancel() {
+    const data = this.infos();
+    if (!data) return;
+
+    this.clubForm.reset(
+      {
+        stadiumName: data.stadiumName,
+        street: data.street,
+        city: data.city,
+        postalCode: data.postalCode,
+        clubName: data.clubName,
+        contactEmail: data.contactEmail,
+      },
+      { emitEvent: false },
+    );
+
+    this.clubForm.markAsPristine();
+    this.clubForm.markAsUntouched();
+    this.clubForm.disable();
+    this.editIsClicked.set(false);
+  }
+
+  isInvalid(name: string): boolean {
+    const control = this.clubForm.get(name);
+    return !!(control && control.invalid && (control.touched || control.dirty));
   }
 }
